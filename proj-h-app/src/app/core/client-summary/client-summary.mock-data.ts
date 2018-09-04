b8175a6bcee92fdca2edce23e356cf015308f4b9
Francois Ferreira <francois19844@gmail.com>
2018-09-04 22:21:22 +0200

    Getting Client mock data done, continued with summary styles (WIP)

--- proj-h-app/src/app/core/client-summary/client-summary.mock-data.ts
+++ proj-h-app/src/app/core/client-summary/client-summary.mock-data.ts
@@ -5,6 +5,6 @@ export const CLIENT: ClientSummary = {
     name: "Mr. Voyu Makatini",
     location: "Stellenbosch",
     occupation: "Cashier at Pick 'n Pay",
-    birthDate: new Date("27/09/1984"),
+    birthDate: new Date('1984-09-27:00:00:00Z'),
     score: 2 
 };
