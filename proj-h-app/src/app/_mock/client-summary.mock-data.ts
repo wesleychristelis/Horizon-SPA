e7655ecf2cfb1f0d0853b6ea6028772267134250
Wesley Christelis <wesley.christelis@gmail.com>
2018-09-21 14:53:09 +0200

    WIP: Bootstrap containers and  grid system

--- proj-h-app/src/app/_mock/client-summary.mock-data.ts
+++ proj-h-app/src/app/_mock/client-summary.mock-data.ts
@@ -2,14 +2,14 @@ import { ClientSummary } from '../_models/client-summary';
  
 export const CLIENT: ClientSummary = {
     id: 11, 
-    name: "Gollum",
-    location: "Middle Earth",
-    occupation: "Gaurder of the precious",
+    name: "Deadpool",
+    location: "Chicago",
+    occupation: "Mercenary",
     birthDate: new Date('1984-09-27:00:00:00Z'),
     score: 4,
     photo: {
         id:1,
-        url: "../../assets/Image9.jpg",
+        url: "../../assets/images.png",
         description: "Default Icon",
         dateAdded: new Date(),
         isMain: true
